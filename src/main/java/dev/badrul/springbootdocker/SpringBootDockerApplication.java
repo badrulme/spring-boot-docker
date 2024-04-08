2bb5a75433bc59b3ed489ed96953281b3b1a4818
Badrul <badrul.alam@bjitgroup.com>
2024-04-08 11:11:58 +0600

    Initial commit

---
+++ src/main/java/dev/badrul/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package dev.badrul.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+}
